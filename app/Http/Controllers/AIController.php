255100e69f13b54d6f153793df21833c6860e7ad
Sajid Ahamed <sajid13673@gmail.com>
2025-09-07 22:00:28 +0530

    feat(chat): enhance generate logic to store conversation and associated user/chatbot messages

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Conversation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Exception;
@@ -11,8 +12,8 @@ class AIController extends Controller
     public function generate(Request $request)
     {
         try {
+            $user = $request->user();
             $message = $request->input('message', 'Hello AI!');
-
             $response = Http::withHeaders([
                 'Authorization' => 'Bearer ' . config('services.cohere.token'),
                 'Content-Type' => 'application/json',
@@ -27,14 +28,37 @@ class AIController extends Controller
                     'error' => $response->json() ?? $response->body(),
                 ], $response->status());
             }
+            $conversation = Conversation::find($request->conversation_id);
+            if (!$conversation) {
+                $titleResponse = Http::withHeaders([
+                    'Authorization' => 'Bearer ' . config('services.cohere.token'),
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json'
+                ])->post('https://api.cohere.com/v1/chat', [
+                    'message' => 'generate a short title for this message : ' . $message
+                ]);
 
-            $data = $response->json();
+                if ($titleResponse->failed()) {
+                    return response()->json([
+                        'status' => false,
+                        'error' => $titleResponse->json() ?? $titleResponse->body(),
+                    ], $titleResponse->status());
+                }
+                $title = $titleResponse->json('text');
+                $conversation = $user->conversations()->create(["title" => $title]);
+            }
+            $reply = $response->json('text');
+            $conversation->messages()->createMany([
+                ['role' => 'USER', 'message' => $message],
+                ['role' => 'CHATBOT', 'message' => $reply],
+            ]);
 
             return response()->json([
                 'status' => true,
-                'result' => $data['text'] ?? 'No output',
+                'message' => $message,
+                'reply' => $reply,
+                'conversation' => $conversation
             ]);
-
         } catch (Exception $e) {
             return response()->json([
                 'status' => false,

f94d9099e65ce7508a7edfa716e002fd1cc1d3b1
Sajid Ahamed <sajid13673@gmail.com>
2026-09-18 15:53:26 +0530

    fix(cors): configure allowed origins and restrict HTTP methods

--- config/cors.php
+++ config/cors.php
@@ -4,9 +4,11 @@ return [
 
     'paths' => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => array_filter(
+        array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', '')))
+    ),
 
     'allowed_origins_patterns' => [],
 
